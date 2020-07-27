14f90698a062ba4c85c99e1305b1a73ebf719c6b
Andre Battle <batldre7@gmail.com>
2020-07-27 19:56:13 +0000

    Formatting errors

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -3,11 +3,17 @@ import {FlyControls} from 'https://threejs.org/examples/jsm/controls/FlyControls
 import {GUI} from 'https://threejs.org/examples/jsm/libs/dat.gui.module.js';
 
 // global objects
-let scene, camera, renderer, clock, controls;
-let rotationData = new Map();
-let points = [];
+let scene;
+let camera;
+let renderer;
+let clock;
+let controls;
+const rotationData = new Map();
+const points = [];
 
-
+/** 
+ * Intializes the scene and its child objects
+ */
 function init() {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
@@ -29,7 +35,7 @@ function init() {
   let geometry = new THREE.PlaneGeometry(5, 25, 1);
   let material =
       new THREE.MeshBasicMaterial({color: 'pink', side: THREE.DoubleSide});
-  let plane = new THREE.Mesh(geometry, material);
+  const plane = new THREE.Mesh(geometry, material);
   plane.rotation.x = Math.PI / 2;
   plane.position.z = -10;
   scene.add(plane);
@@ -37,33 +43,33 @@ function init() {
   // Fake Pose Data
   material = new THREE.LineBasicMaterial({color: 'red'});
   for (let increment = 0; increment < 15; increment++) {
-    pnoints.push(new THREE.Vector3(
-        Math.pow(-1, increment), .20, -increment));  // GPS points
+    points.push(new THREE.Vector3(
+        Math.pow(-1, increment), .20, -increment));// GPS points
 
-    let geometry = new THREE.CylinderGeometry(.02, .02, .5);
-    let cylinder = new THREE.Mesh(geometry, material);
+    const geometry = new THREE.CylinderGeometry(.02, .02, .5);
+    const cylinder = new THREE.Mesh(geometry, material);
     cylinder.rotateX(THREE.Math.degToRad(-90));
     cylinder.position.set(0, 0, -.25);
-    scene.add(cylinder);  // Orientation indicator
+    scene.add(cylinder);// Orientation indicator
 
-    let pivot = new THREE.Group();  // Pivot point for each cylinder
+    const pivot = new THREE.Group();// Pivot point for each cylinder
     pivot.position.set(Math.pow(-1, increment), .25, -increment);
-      scene.add(pivot);
+    scene.add(pivot);
     pivot.add(cylinder);
-    let pivotSphereGeo =
-        new THREE.SphereGeometry(.03);  // Small sphere indicating pivot point
-    let pivotSphere = new THREE.Mesh(pivotSphereGeo);
+    const pivotSphereGeo =
+      new THREE.SphereGeometry(.03);// Small sphere indicating pivot point
+    const pivotSphere = new THREE.Mesh(pivotSphereGeo);
     pivotSphere.position.set(Math.pow(-1, increment), .25, -increment);
     scene.add(pivotSphere);
 
     rotationData.set(increment, {origin: pivot, mesh: cylinder});
-  }ppp
+  }
   geometry = new THREE.BufferGeometry().setFromPoints(points);
   material = new THREE.LineBasicMaterial({color: 'blue'});
-  let line = new THREE.Line(geometry, material);
+  const line = new THREE.Line(geometry, material);
   scene.add(line);
   // GUI initialization
-  let gui = new GUI();
+  const gui = new GUI();
   let params = {clipIntersection: true, Yaw: 0, showHelpers: false};
   gui.add(params, 'Yaw', -360, 360)
       .step(.1)
@@ -95,9 +101,9 @@ function init() {
       });
 }
 
-let animate = function() {
+function animate() {
   requestAnimationFrame(animate);
-  let delta = clock.getDelta();
+  const delta = clock.getDelta();
   controls.update(delta);
   renderer.render(scene, camera);
 };
