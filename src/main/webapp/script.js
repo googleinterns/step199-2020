06205e2e09535cedab0fb8821b04adbbdf2a947b
Andre Battle <batldre7@gmail.com>
2020-07-31 19:35:59 +0000

    Temporary commit

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,15 +1,31 @@
 
 import {FlyControls} from 'https://threejs.org/examples/jsm/controls/FlyControls.js';
 import {GUI} from 'https://threejs.org/examples/jsm/libs/dat.gui.module.js';
-
+import {test} from '/decoded.js';
 // Initialization of global objects.
 let scene;
 let camera;
 let renderer;
 let clock;
 let controls;
+let count = test.data.length;
 const rotationData = new Map();
-const points = [];
+const path = [];
+
+let highest = 0;
+let lowest = 0;
+for(let i = 0; i<test.data.length;i++){
+    if(highest<test.data[i].pose.long){
+        highest = test.data[i].pose.long;
+    }    
+    if(lowest>test.data[i].pose.long){
+        lowest = test.data[i].pose.long;
+    }  
+}
+console.log("Highest: " + highest);
+console.log("Lowest: " + lowest);
+console.log("Range: " + (highest - lowest));
+
 
 /**
  * Initializes the scene, camera, renderer, and clock.
@@ -29,19 +45,20 @@ function init() {
 
   // The camera controls allows the user to fly with the camera.
   controls = new FlyControls(camera, renderer.domElement);
-  controls.movementSpeed = 1;
+  controls.movementSpeed = 5;
   controls.rollSpeed = Math.PI / 10;
   controls.autoForward = false;
   controls.dragToLook = true;
 
-  addObjects();
-  gui();
+  addPoseData();
 }
 
 /**
- * Creates all the objects and adds them to the scene.
+ * Creates all the objects from pose data and adds them to the scene.
 */
-function addObjects() {
+function addPoseData() {
+  let dummy = new THREE.Object3D;
+  dummy.matrixAutoUpdate = false;
   let geometry = new THREE.PlaneGeometry(5, 25, 1);
   let material =
       new THREE.MeshBasicMaterial({color: 'pink', side: THREE.DoubleSide});
@@ -50,71 +67,58 @@ function addObjects() {
   plane.position.z = -10;
   scene.add(plane);
 
-  // Fake Pose Data
-  material = new THREE.LineBasicMaterial({color: 'red'});
-  for (let increment = 0; increment < 15; increment++) {
-    points.push(new THREE.Vector3(
-        Math.pow(-1, increment), .20, -increment));// GPS points
-
-    const geometry = new THREE.CylinderGeometry(.02, .02, .5);
-    const cylinder = new THREE.Mesh(geometry, material);
-    cylinder.rotateX(THREE.Math.degToRad(-90));
-    cylinder.position.set(0, 0, -.25);
-    scene.add(cylinder);// Orientation indicator
-
-    const pivot = new THREE.Group();// Pivot point for each cylinder
-    pivot.position.set(Math.pow(-1, increment), .25, -increment);
-    scene.add(pivot);
-    pivot.add(cylinder);
-    const pivotSphereGeo =
-      new THREE.SphereGeometry(.03);// Small sphere indicating pivot point
-    const pivotSphere = new THREE.Mesh(pivotSphereGeo);
-    pivotSphere.position.set(Math.pow(-1, increment), .25, -increment);
-    scene.add(pivotSphere);
-
-    rotationData.set(increment, {origin: pivot, mesh: cylinder});
+  // Adds test pose data as one Geometry instance
+  for (let increment = 0; increment < count; increment++) {
+    path.push(new THREE.Vector3(
+        (test.data[increment].pose.lat/1000) - 1140885,
+        test.data[increment].pose.alt * 100,
+        test.data[increment].pose.long * 100));// GPS points
   }
-  geometry = new THREE.BufferGeometry().setFromPoints(points);
+  geometry = new THREE.BufferGeometry().setFromPoints(path);
   material = new THREE.LineBasicMaterial({color: 'blue'});
   const line = new THREE.Line(geometry, material);
   scene.add(line);
+
+  let points = [];
+  points.push(new THREE.Vector3(0, 0, 0))
+  points.push(new THREE.Vector3(0, .5, 0));
+  material = new THREE.MeshBasicMaterial({color: 'red'});
+  geometry = new THREE.CylinderBufferGeometry(.02, .02, 1);
+  const direction = new THREE.InstancedMesh(geometry, material, count);
+  direction.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
+  scene.add(direction);// Orientation indicator
+
+  for(let i = 0; i < count; i++){
+    // var pivotSphereGeo = new THREE.SphereGeometry(.02); // Small sphere indicating pivot point
+    // var pivotSphere = new THREE.Mesh(pivotSphereGeo);
+    // pivotSphere.position.set(
+    //   (test.data[i].pose.lat/1000) - 1140885,
+    //   test.data[i].pose.alt * 100,
+    //   test.data[i].pose.long * 100);
+    // scene.add(pivotSphere);
+
+    let matrix = new THREE.Matrix4();
+    dummy.matrix.identity();
+    matrix.makeTranslation(
+      (test.data[i].pose.lat/1000) - 1140885,
+      test.data[i].pose.alt * 100,
+      test.data[i].pose.long * 100);
+    matrix.multiply(new THREE.Matrix4().makeRotationX(-Math.PI/2));
+    matrix.multiply(new THREE.Matrix4().makeRotationX(
+        test.data[i].head.pitchDeg));
+    matrix.multiply(new THREE.Matrix4().makeRotationY(
+        test.data[i].head.yawDeg));
+    matrix.multiply(new THREE.Matrix4().makeRotationZ(
+        test.data[i].head.rollDeg));
+    matrix.multiply(new THREE.Matrix4().makeTranslation(0.0, .5, 0.0));
+    dummy.applyMatrix4(matrix);
+    dummy.updateMatrix();
+    direction.setMatrixAt(i, dummy.matrix);
+  }
+  direction.instanceMatrix.needsUpdate = true;   
 }
 
-/**
- * Creates a GUI for manipulating the orientation of each data point
- */
-function gui() {
-  const gui = new GUI();
-  let params = {clipIntersection: true, Yaw: 0, showHelpers: false};
-  gui.add(params, 'Yaw', -360, 360)
-      .step(.1)
-      .name('Yaw (degrees)')
-      .onChange(function(value) {
-        for (let i = 0; i < rotationData.size; i++) {
-          rotationData.get(i).origin.rotation.y = THREE.Math.degToRad(value);
-        }
-      });
 
-  params = {clipIntersection: true, Pitch: 0, showHelpers: false};
-  gui.add(params, 'Pitch', -360, 360)
-      .step(.1)
-      .name('Pitch (degrees)')
-      .onChange(function(value) {
-        for (let i = 0; i < rotationData.size; i++) {
-          rotationData.get(i).origin.rotation.x = THREE.Math.degToRad(value);
-        }
-      });
-
-  params = {clipIntersection: true, Roll: 0, showHelpers: false};
-  gui.add(params, 'Roll', -360, 360)
-      .step(.1)
-      .name('Roll (degrees)')
-      .onChange(function(value) {
-        for (let i = 0; i < rotationData.size; i++) {
-          rotationData.get(i).origin.rotation.z = THREE.Math.degToRad(value);
-        }
-      });
-}
 /**
  * This is the animation loop which continually updates the scene.
  * It allows the movement of objects to be seen on screen and the camera
