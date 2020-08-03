5c5cc283e7cb84296700fc05c6b5d4a84db06acf
David Morley <dmoe22@outlook.com>
2020-08-03 15:36:08 -0700

    Added forgotten file

---
+++ src/main/java/data/Database.java
@@ -0,0 +1,22 @@
+/* This class creates an instance of a database that a file can be written to and read from.
+Assumptions: for each runID, only 1 of each datatype can be associated with it */
+package data;
+public class Database{
+ 
+    /*place where files are stored*/
+    private String name;
+ 
+    /* Creates instance of a database with this name. */
+    public Database(String name){
+      /* Database will be GCS and buckets. Bucket will named by different runids. */
+      this.name = name;
+    }
+    
+    /* Returns projectID of Database. */
+    public String getName(){
+        return name; 
+    }
+    
+
+   
+}
