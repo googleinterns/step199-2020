e7239740c46bd6edb97fbb397b3cea918aed785d
David Morley <dmoe22@outlook.com>
2020-08-04 23:41:19 -0700

    Add database fetch, change json return format, add very simple home page for testing.

---
+++ src/main/webapp/script.js
@@ -0,0 +1,15 @@
+function loadUrls() {
+    fetch('/data').then(response => response.json()).then(json => {
+        const links = document.getElementById('myUL');
+        for (const key in json) {
+            let li = document.createElement('li');
+            let att = document.createElement('a');
+            att.textContent = key + ":" + json[key];
+            att.setAttribute('href', "/getrun?id=" + key + "&dataType=" + json[key]);
+            console.log(att);
+            li.appendChild(att);
+            links.appendChild(li);
+
+        }
+    })
+}
