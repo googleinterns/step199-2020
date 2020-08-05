978e3b2397ae44e0a3339b68f04a4851b51e57be
frelicia tucker <freliciatucker@google.com>
2020-08-05 16:46:40 +0000

    more removal

--- src/main/java/shared/sharedObjects.java
+++
@@ -1,7 +0,0 @@
-package shared;
-
-// Variables that need to be accessed between multiple class instances (i.e. Database initialization variable).
-public class sharedObjects {
-    
-    //public static 
-}
