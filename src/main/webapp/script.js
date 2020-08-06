3da2140e2cb21b89cb5273a279aa022d1cb5b8f1
David Morley <dmoe22@outlook.com>
2020-08-06 14:44:49 -0700

    Run autoformatter

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,6 +1,8 @@
+let currentJson;
 function loadUrls() {
     fetch('/data').then(response => response.json()).then(json => {
         const links = document.getElementById('myUL');
+        currentJson = json;
         for (const key in json) {
             let li = document.createElement('li');
             let att = document.createElement('a');
@@ -9,7 +11,6 @@ function loadUrls() {
             console.log(att);
             li.appendChild(att);
             links.appendChild(li);
-
         }
     })
 }
