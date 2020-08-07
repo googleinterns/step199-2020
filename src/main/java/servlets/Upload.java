6b8f4f2197c9f9bd24c27bddd944cc3143edd054
David Morley <dmoe22@outlook.com>
2020-08-06 17:26:44 -0700

    Apply linters

--- src/main/java/servlets/Upload.java
+++ src/main/java/servlets/Upload.java
@@ -1,9 +1,9 @@
 package servlets;
 
+import IO.WriterSimple;
+import encoder.Encoder;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collection;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -11,40 +11,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-
-import IO.WriterSimple;
-import encoder.Encoder;
 import shared.sharedObjects;
 
-/**
- * Fetch the given data points for a specific run based on its runId and the
- * dataType.
- */
+/** Fetch the given data points for a specific run based on its runId and the dataType. */
 @MultipartConfig
 @WebServlet("/upload")
 public class Upload extends HttpServlet {
-    // Return numEntries runId and name pairs for getting urls to load viewer data.
-    // Optional numEntries parameter limits the number of returned values.
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException, IllegalStateException {
-        response.setContentType("text/html;");
+  // Return numEntries runId and name pairs for getting urls to load viewer data.
+  // Optional numEntries parameter limits the number of returned values.
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException, IllegalStateException {
+    response.setContentType("text/html;");
 
-        String dataType = "pose";
-        // Should take in database instance, along with data type.
-        WriterSimple dataWriter = new WriterSimple(sharedObjects.dataInstance, dataType);
-        Part filePart = null;
+    String dataType = "pose";
+    // Should take in database instance, along with data type.
+    WriterSimple dataWriter = new WriterSimple(sharedObjects.dataInstance, dataType);
+    Part filePart = null;
 
-        filePart = request.getPart("file");
-        // Get the input stream and encode it/store it in the given OutputStream.
-        InputStream uploadedFile = filePart.getInputStream();
-        Encoder.encode(uploadedFile, dataWriter.write());
-        System.out.println("We whould now be done");
-        // We now make would make an instance of the reader object to get a stream from
-        // the database, then pass this value to the decoder, with a string as our ouput
-        // stream.
-        // As reader object is not yet defined instead get the file input stream
-        // directly
-    }
-
+    filePart = request.getPart("file");
+    // Get the input stream and encode it/store it in the given OutputStream.
+    InputStream uploadedFile = filePart.getInputStream();
+    Encoder.encode(uploadedFile, dataWriter.write());
+    System.out.println("We whould now be done");
+    // We now make would make an instance of the reader object to get a stream from
+    // the database, then pass this value to the decoder, with a string as our ouput
+    // stream.
+    // As reader object is not yet defined instead get the file input stream
+    // directly
+  }
 }
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,15 +1,22 @@
+/**
+* Load links in a list for all runs in the current database
+* @return {void}
+*/
 function loadUrls() {
-    fetch('/data').then(response => response.json()).then(json => {
-        const links = document.getElementById('myUL');
-        for (const key in json) {
-            let li = document.createElement('li');
-            let att = document.createElement('a');
-            att.textContent = key + ":" + json[key];
-            att.setAttribute('href', "/getrun?id=" + key + "&dataType=" + json[key]);
-            console.log(att);
-            li.appendChild(att);
-            links.appendChild(li);
-
-        }
-    })
+  fetch('/data').then((response) => response.json()).then((json) => {
+    const links = document.getElementById('myUL');
+    for (const key in json) {
+      if (Object.prototype.hasOwnProperty.call(json, key)) {
+        const li = document.createElement('li');
+        const att = document.createElement('a');
+        att.textContent = key + ':' + json[key];
+        att.setAttribute('href', '/getrun?id=' +
+                    key + '&dataType=' + json[key]);
+        console.log(att);
+        li.appendChild(att);
+        links.appendChild(li);
+      }
+    }
+  });
 }
+loadUrls();
