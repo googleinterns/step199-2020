323424cb4b7684b0aac58c1afbd4e29e42d50ba1
frelicia tucker <freliciatucker@google.com>
2020-08-13 02:40:42 +0000

    adds interface

--- src/main/java/data/Database.java
+++ src/main/java/data/Database.java
@@ -1,6 +1,5 @@
 package data;
 
-import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -15,12 +14,5 @@ public interface Database {
 
   public ArrayList<String> getAllFiles();
 
-  /*
-   * TODO: I dont really want to include this in the interface,
-   * but i need to delete directories after i make them in tests.
-   * Since these tests are run locally, i know im using the file system
-   * implementation, so i need a method to give me acces to the file/directory
-   * in order to then delete it.
-   */
-  public File getDatabase();
+  public void delete();
 }
