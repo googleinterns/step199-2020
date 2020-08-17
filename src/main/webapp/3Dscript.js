408d0baca6dec805114e8c343e703a3ebf191ab1
Andre Battle <batldre7@gmail.com>
2020-08-17 22:12:24 +0000

    Changed variable names, added comments, changed magic numbers.

--- src/main/webapp/3Dscript.js
+++ src/main/webapp/3Dscript.js
@@ -10,11 +10,12 @@ let clock;
 let controls;
 let orientation;
 let trajectory;
-let dataLength;
 let pose;
-let poseRotation = {degrees: 0};
-let poseScalar = {multiplier: 1};
-let posePosition = {x: 0, z: 0};
+let poseTransform = {
+  translateX: 0,
+  translateY: 0,
+  rotate: 0,
+  scale: 0};
 const apiKey = 'AIzaSyDCgKca9sLuoQ9xQDfHUvZf1_KAv06SoTU';
 
 initThreeJs();
@@ -56,7 +57,7 @@ function fetchData() {
     fetch('/getrun?id=' + id + '&dataType=' + type)
     .then(response => response.json())
     .then(data => pose = data)
-    .then(() => addTerrian());
+    .then(() => addMap());
 }
 
 /**
@@ -65,19 +66,18 @@ function fetchData() {
  * request and significantly cut down rendering time with 
  * geometry instancing.
 */
-function addTerrian() {
-  // Creates the 2D terrain.
-  dataLength = pose.length;
+function addMap() {
+  // Creates the 2D map.
   let loader = new THREE.TextureLoader();
   let material = new THREE.MeshLambertMaterial({ 
     map: loader.load(
       'https://maps.googleapis.com/maps/api/staticmap' +
-      '?format=png&center=48.129872,11.582905&zoom=18&size=500x500&key='
-      + apiKey)
+      '?format=png&center=' + pose[0].lat + ',' + pose[0].lng + 
+      '&zoom=18&size=500x500&key=' + apiKey)
     });
   let geometry = new THREE.PlaneGeometry(50,50);
   const map = new THREE.Mesh(geometry, material);
-  map.position.set(0,0,0);
+  map.position.set(0,-4,0);
   map.rotation.x = -Math.PI / 2;
   scene.add(map);
 
@@ -85,13 +85,12 @@ function addTerrian() {
   let light = new THREE.PointLight(0xffffff, 1, 0 );
   light.position.set(0, 100, 0);
   scene.add(light)
-  console.log(dataLength);
   plotTrajectory();
   
   // Adds pose data to the scene as one Geometry instance.
   material = new THREE.MeshBasicMaterial({color: 'red'});
   geometry = new THREE.CylinderBufferGeometry(.005, .005, .1);
-  orientation = new THREE.InstancedMesh(geometry, material, dataLength);
+  orientation = new THREE.InstancedMesh(geometry, material, pose.length);
   orientation.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
   scene.add(orientation);
   plotOrientation();
@@ -105,52 +104,54 @@ function plotTrajectory() {
   // Removes any existing trajectory objects for repositioning.
   scene.remove(trajectory);
   let coordinates=[];
-  for (let increment = 0; increment < dataLength; increment++) {
+  for (point of pose) {
     coordinates.push(new THREE.Vector3(
-      (pose[increment].lng-11.582905) * 25000 * poseScalar.multiplier,
-      (pose[increment].alt - 6.582905)/4,
-      (pose[increment].lat-48.129872) * 25000 * -poseScalar.multiplier));// GPS points
+      (point.lng - pose[0].lng) * 25000 * poseTransform.scale,
+      (point.alt - pose[0].alt)/4,
+      (point.lat - pose[0].lat) *25000 * -poseTransform.scale)
+    );
   }
   const geometry = new THREE.BufferGeometry().setFromPoints(coordinates);
   const material = new THREE.LineBasicMaterial({color: 'blue'});
   trajectory = new THREE.Line(geometry, material);
   scene.add(trajectory);
-  trajectory.position.x = posePosition.x;
-  trajectory.position.z = posePosition.z;
-  trajectory.rotation.y = THREE.Math.degToRad(poseRotation.degrees);
+  trajectory.position.x = poseTransform.translateX;
+  trajectory.position.z = poseTransform.translateY;
+  trajectory.rotation.y = THREE.Math.degToRad(poseTransform.rotate);
 }
 
 /**
  * This repositions all children the orientation object. The object 
  * is a geometry instance, initializing the object with one geometry
- * call rather than rather than the number of data points. (O(1) vs. O(N))
+ * call rather than the number of data points. (O(1) vs. O(N))
  */
 function plotOrientation() {
-  let dummy = new THREE.Object3D;
-  dummy.matrixAutoUpdate = false;
-  for(let i = 0; i < dataLength; i++){
+  for (point of pose){
+    /**
+     * Matrix is a 4x4 matrix used to translate and rotate each instance
+     * of the pose orientation locally instead using the world transform.
+     * This means it will rotate relative to the position of the matrix, 
+     * not the center of the 3D scene.
+     */ 
     let matrix = new THREE.Matrix4();
-    dummy.matrix.identity();
     matrix.makeTranslation(
-      (pose[i].lng-11.582905) * 25000 * poseScalar.multiplier,
-      (pose[i].alt - 6.582905)/4,
-      (pose[i].lat-48.129872) * 25000 * -poseScalar.multiplier);
+      (point.lng - pose[0].lng) * 25000 * poseTransform.scale,
+      (point.alt - pose[0].alt)/4,
+      (point.lat - pose[0].lat) * 25000 * -poseTransform.scale);
     matrix.multiply(new THREE.Matrix4().makeRotationX(-Math.PI/2));
     matrix.multiply(new THREE.Matrix4().makeRotationX(
-        THREE.Math.degToRad(pose[i].pitchDeg)));
+        THREE.Math.degToRad(point.pitchDeg)));
     matrix.multiply(new THREE.Matrix4().makeRotationZ(
-       THREE.Math.degToRad(pose[i].yawDeg)));
+       THREE.Math.degToRad(point.yawDeg)));
     matrix.multiply(new THREE.Matrix4().makeRotationY(
-        THREE.Math.degToRad(pose[i].rollDeg)));
+        THREE.Math.degToRad(point.rollDeg)));
     matrix.multiply(new THREE.Matrix4().makeTranslation(0.0, .05, 0.0));
-    dummy.applyMatrix4(matrix);
-    dummy.updateMatrix();
-    orientation.setMatrixAt(i, dummy.matrix);
+    orientation.setMatrixAt(i, matrix);
   }
   orientation.instanceMatrix.needsUpdate = true; 
-  orientation.position.x = posePosition.x;
-  orientation.position.z = posePosition.z;
-  orientation.rotation.y = THREE.Math.degToRad(poseRotation.degrees);
+  orientation.position.x = poseTransform.translateX;
+  orientation.position.z = poseTransform.translateY;
+  orientation.rotation.y = THREE.Math.degToRad(poseTransform.rotate);
 }
 
 /**
@@ -159,13 +160,13 @@ function plotOrientation() {
  */
 function gui() {
   const gui = new GUI();
-  gui.add(poseRotation, 'degrees', 0, 360,1).onChange(plotOrientation)
+  gui.add(poseTransform, 'rotate', 0, 360,1).onChange(plotOrientation)
   .onFinishChange(plotTrajectory).name('Pose Rotation (degrees)');
-  gui.add(posePosition, 'x', -10, 10,.025).onChange(plotOrientation)
+  gui.add(poseTransform, 'translateX', -10, 10,.025).onChange(plotOrientation)
   .onFinishChange(plotTrajectory).name('X Axis Translation');
-  gui.add(posePosition, 'z', -10, 10,.025).onChange(plotOrientation)
+  gui.add(poseTransform, 'transformY', -10, 10,.025).onChange(plotOrientation)
   .onFinishChange(plotTrajectory).name('Y Axis Translation');
-  gui.add(poseScalar, 'multiplier',.5, 2,.25).onChange(plotOrientation)
+  gui.add(poseTransform, 'scale',.5, 2,.25).onChange(plotOrientation)
   .onFinishChange(plotTrajectory).name('Pose Scale Multiplier');
 }
 
