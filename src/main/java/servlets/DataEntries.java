8fbad401cdf6c452e196fbe37d16c5e9830d46a9
frelicia tucker <freliciatucker@google.com>
2020-08-18 20:42:10 +0000

    repositions databaseQuery

--- src/main/java/servlets/DataEntries.java
+++ src/main/java/servlets/DataEntries.java
@@ -1,12 +1,7 @@
 package servlets;
 
-import com.google.appengine.repackaged.com.google.gson.Gson;
-import com.google.appengine.repackaged.com.google.gson.GsonBuilder;
-import data.Database;
-import java.io.File;
+import data.DatabaseQuery;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.TreeSet;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,72 +14,25 @@ import shared.sharedObjects;
  */
 @WebServlet("/data")
 public class DataEntries extends HttpServlet {
-  private Database dataInstance = new Database(sharedObjects.databaseName);
-
-  // Get a list of the data values that are currently in the database. Take an optional number of
-  // entries parameter.
+  // Return numEntries runId and name pairs for getting urls to load viewer data.
+  // Optional numEntries parameter limits the number of returned values.
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html");
+    response.setContentType("text/html;");
     String entriesUnparsed = request.getParameter("numEntries");
     // If the numEntries value is not set default to getting all the entries.
     int numEntries = -1;
     if (entriesUnparsed != null) {
       try {
         numEntries = Integer.parseInt(entriesUnparsed);
-      } catch (NumberFormatException e) {
+      } catch (Exception e) {
         System.err.println("Invalid value provided stick to default");
         numEntries = -1;
       }
     }
     // Logic that goes here that calls two methods from the intermediate Database
-    // depending on what exactly.
-    String json = getJson();
+    // depending on what exactly
+    String json = DatabaseQuery.getJson(sharedObjects.dataInstance);
     response.getWriter().println(json);
-  }
-
-  // A temporary method to replace the database until it is added in the near
-  // future.
-  private String getJson() {
-    // In the future this will be implemented by calling a method from the database
-    // class. For now this is implemented as reading from a file. Although the name
-    // could be recovered from the shared instance, this implementation will be
-    // changed when merged into Frelica's database implementation and this function
-    // will no longer be called.
-    String fileName = dataInstance.getName() + "/";
-    // Remove extraneous whitespace from the name.
-    File dir = new File(fileName);
-    System.out.println(dir.getAbsolutePath());
-    File[] dataEntries = dir.listFiles();
-    if (dataEntries == null) {
-      System.out.println("Directory is empty");
-      return "{}";
-    }
-
-    // Associate a runID with a list (for json) of the entry types that are found
-    // for it.
-    HashMap<String, TreeSet<String>> idToDataTypes = new HashMap<String, TreeSet<String>>();
-    for (File file : dataEntries) {
-      // Parse through all entries and write them as JSON.
-      String name = file.getName();
-      // Use regex to extract name and type from string.
-      String[] params = name.split("[_.]");
-      if (params.length != 3) {
-        System.err.println("Invalid database entry format!");
-        return "";
-      }
-      String runId = params[0];
-      String type = params[1];
-
-      // Add empty TreeSet to map if none is present.
-      TreeSet<String> typeList = idToDataTypes.computeIfAbsent(runId, id -> new TreeSet<String>());
-      // Add specific entry.
-      typeList.add(type);
-    }
-    System.out.println(idToDataTypes);
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    String json = gson.toJson(idToDataTypes);
-    System.out.println(json);
-    return json;
   }
 }
