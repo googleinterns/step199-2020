51550d2c10b94cf6b92c559dc4c32b57c2a7cb80
frelicia tucker <freliciatucker@google.com>
2020-08-18 05:01:10 +0000

    adds Database interface

--- src/main/java/data/Database.java
+++ src/main/java/data/Database.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 
+/* Interface for dealing with any database. */
 public interface Database {
   /* Abstract methods. */
   public String getDatabaseName();
