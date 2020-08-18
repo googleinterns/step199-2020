860b5bb918d46090c740e75f0134f7bf2fd090e2
frelicia tucker <freliciatucker@google.com>
2020-08-18 20:54:20 +0000

    makes database interface

--- src/main/java/data/Database.java
+++ src/main/java/data/Database.java
@@ -1,79 +1,19 @@
 package data;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 
-/* This class creates an instance of a database that a file can be written to and read from.
-Assumptions: for each runID, only 1 of each datatype can be associated with it. */
-public class Database {
-  /* Place where files are stored.*/
-  private String directory;
+public interface Database {
+  /* Abstract methods. */
+  public String getDatabaseName();
 
-  /* List of all files stored in Database.*/
-  private ArrayList<String> files;
+  public InputStream readData(String runID, String type) throws IOException;
 
-  /* Creates instance of a database with this name. */
-  public Database(String name) {
-    this.directory = name;
-    files = new ArrayList<String>();
-  }
+  public OutputStream writeData(String runId, String type) throws IOException;
 
-  /* Returns name associated with Database. */
-  public String getDatabaseName() {
-    return directory;
-  }
+  public ArrayList<String> getAllFiles();
 
-  /*
-   * Attempt to open and return a stream to the data, normally to database, in this case to
-   * file. only reaches null return if attempt fails.
-   */
-  public InputStream readData(String runID, String type) {
-    String fileName = makeFileName(runID, type);
-    try {
-      return new FileInputStream(fileName);
-    } catch (FileNotFoundException e) {
-      System.err.format("Unable to locate the give file with name %s", fileName);
-    }
-    return null;
-  }
-
-  /*
-   * Attempt to open a stream to the data, normally to database, in this case to
-   * file. only reaches null return if attempt fails.
-   */
-  public OutputStream writeData(String runId, String type) {
-    String fileName = makeFileName(runId, type);
-    try {
-      return new FileOutputStream(fileName);
-    } catch (FileNotFoundException e) {
-      System.err.format("Unable to locate the give file with name %s", fileName);
-    }
-    return null;
-  }
-
-  private String makeFileName(String runId, String type) {
-    return getDatabaseName() + "/" + runId + "_" + type;
-  }
-
-  /* Adds file to database given its runid and type. */
-  public String newDatabaseEntry(String runId, String type) {
-    String fileName = makeFileName(runId, type);
-    files.add(fileName);
-    return fileName;
-  }
-
-  /* Return name of file with runid and type in database. */
-  public String findName(String runId, String type) {
-    String fileName = getDatabaseName() + "/" + runId + "_" + type;
-    return files.get(0);
-  }
-
-  /* Returns list of files in database. */
-  public ArrayList<String> getAllFiles() {
-    return files;
-  }
+  public void delete();
 }
